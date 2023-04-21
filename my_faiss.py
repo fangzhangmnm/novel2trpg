ff0482d0c2e7a446d8a6cc3b618f4f274052f1d1
fangzhangmnm <1561797062@qq.com>
2023-04-20 22:37:46 -0400

    working on generate training data again

--- my_faiss.py
+++ my_faiss.py
@@ -10,13 +10,18 @@ class SentenceTransformerNormalized(SentenceTransformer):
 embeddings = HuggingFaceEmbeddings(model_name=r'D:\ml\text2vec-large-chinese')
 embeddings.client=SentenceTransformerNormalized(embeddings.model_name,device="cuda")
 
-def get_bg_hint(context,max_text_len=500,k=25):
+def get_bg_hint(context,max_text_len=500,k=25,show_score=False):
     results=get_bg_hint.vector_store.similarity_search_with_relevance_scores(context,k=k)
     all_result_text=''
     for result,score in results:
-        knowledge_item='【{0}（{1:.2f}）】'.format(result.page_content,score)
+        if show_score:
+            knowledge_item='【{0}（{1:.2f}）】'.format(result.page_content,score)
+        else:
+            knowledge_item='【{0}】'.format(result.page_content)
         if len(all_result_text)+len(knowledge_item)+1>max_text_len:
             break
         all_result_text=all_result_text+knowledge_item+'\n'
+    if all_result_text[-1]=='\n':
+        all_result_text=all_result_text[:-1]
     return all_result_text
 get_bg_hint.vector_store=None
